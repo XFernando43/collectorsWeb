f24affc8a26d2f54cf5c242546398a48a3e0dff4
Fernando Aquino <102422211+XFernando43@users.noreply.github.com>
2024-08-20 10:09:12 -0500

    add: table from angular material

--- src/app/collectors/components/collector-table/collector-table.component.ts
+++ src/app/collectors/components/collector-table/collector-table.component.ts
@@ -2,140 +2,128 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+
+
+export interface PeriodicElement {
+  nombre: string;
+  apellido: string;
+  nroSolicitud: number;
+  docIdentidad: string;
+  status: string;
+  fechaCobranza: string;
+}
+
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {
+    nroSolicitud: 1,
+    nombre: 'Juan',
+    apellido: 'Pérez',
+    docIdentidad: '12345678A',
+    status: 'Pendiente',
+    fechaCobranza: '2024-08-15'
+  },
+  {
+    nroSolicitud: 2,
+    nombre: 'Ana',
+    apellido: 'López',
+    docIdentidad: '87654321B',
+    status: 'Pagado',
+    fechaCobranza: '2024-07-22'
+  },
+  {
+    nroSolicitud: 3,
+    nombre: 'Carlos',
+    apellido: 'García',
+    docIdentidad: '11223344C',
+    status: 'Pendiente',
+    fechaCobranza: '2024-09-01'
+  },
+  {
+    nroSolicitud: 3,
+    nombre: 'Carlos',
+    apellido: 'García',
+    docIdentidad: '11223344C',
+    status: 'Pendiente',
+    fechaCobranza: '2024-09-01'
+  },      {
+    nroSolicitud: 3,
+    nombre: 'Carlos',
+    apellido: 'García',
+    docIdentidad: '11223344C',
+    status: 'Pendiente',
+    fechaCobranza: '2024-09-01'
+  },      {
+    nroSolicitud: 3,
+    nombre: 'Carlos',
+    apellido: 'García',
+    docIdentidad: '11223344C',
+    status: 'Pendiente',
+    fechaCobranza: '2024-09-01'
+  },      {
+    nroSolicitud: 3,
+    nombre: 'Carlos',
+    apellido: 'García',
+    docIdentidad: '11223344C',
+    status: 'Pendiente',
+    fechaCobranza: '2024-09-01'
+  },      {
+    nroSolicitud: 3,
+    nombre: 'Carlos',
+    apellido: 'García',
+    docIdentidad: '11223344C',
+    status: 'Pendiente',
+    fechaCobranza: '2024-09-01'
+  },      {
+    nroSolicitud: 3,
+    nombre: 'Carlos',
+    apellido: 'García',
+    docIdentidad: '11223344C',
+    status: 'Pendiente',
+    fechaCobranza: '2024-09-01'
+  },      {
+    nroSolicitud: 3,
+    nombre: 'Carlos',
+    apellido: 'García',
+    docIdentidad: '11223344C',
+    status: 'Pendiente',
+    fechaCobranza: '2024-09-01'
+  },      {
+    nroSolicitud: 3,
+    nombre: 'Carlos',
+    apellido: 'García',
+    docIdentidad: '11223344C',
+    status: 'Pendiente',
+    fechaCobranza: '2024-09-01'
+  },      {
+    nroSolicitud: 3,
+    nombre: 'Carlos',
+    apellido: 'García',
+    docIdentidad: '11223344C',
+    status: 'Pendiente',
+    fechaCobranza: '2024-09-01'
+  },      {
+    nroSolicitud: 3,
+    nombre: 'Carlos',
+    apellido: 'García',
+    docIdentidad: '11223344C',
+    status: 'Pendiente',
+    fechaCobranza: '2024-09-01'
+  },
+];
+
 
 @Component({
   selector: 'app-collector-table',
   standalone: true,
-  imports: [CommonModule,MatIconModule,MatTableModule],
+  imports: [CommonModule,MatIconModule,MatTableModule,MatPaginatorModule],
   templateUrl: './collector-table.component.html',
   styleUrls: ['./collector-table.component.css']
 })
 export class CollectorTableComponent {
-  public deudores: any[];
-  public paginatedDeudores: any[];
-  public pageSize = 10;
-  public currentPage = 0;
-  public totalPages = 0;
 
-  constructor() {
-    this.deudores = [
-      {
-        nroSolicitud: 1,
-        nombre: 'Juan',
-        apellido: 'Pérez',
-        docIdentidad: '12345678A',
-        status: 'Pendiente',
-        fechaCobranza: '2024-08-15'
-      },
-      {
-        nroSolicitud: 2,
-        nombre: 'Ana',
-        apellido: 'López',
-        docIdentidad: '87654321B',
-        status: 'Pagado',
-        fechaCobranza: '2024-07-22'
-      },
-      {
-        nroSolicitud: 3,
-        nombre: 'Carlos',
-        apellido: 'García',
-        docIdentidad: '11223344C',
-        status: 'Pendiente',
-        fechaCobranza: '2024-09-01'
-      },
-      {
-        nroSolicitud: 3,
-        nombre: 'Carlos',
-        apellido: 'García',
-        docIdentidad: '11223344C',
-        status: 'Pendiente',
-        fechaCobranza: '2024-09-01'
-      },      {
-        nroSolicitud: 3,
-        nombre: 'Carlos',
-        apellido: 'García',
-        docIdentidad: '11223344C',
-        status: 'Pendiente',
-        fechaCobranza: '2024-09-01'
-      },      {
-        nroSolicitud: 3,
-        nombre: 'Carlos',
-        apellido: 'García',
-        docIdentidad: '11223344C',
-        status: 'Pendiente',
-        fechaCobranza: '2024-09-01'
-      },      {
-        nroSolicitud: 3,
-        nombre: 'Carlos',
-        apellido: 'García',
-        docIdentidad: '11223344C',
-        status: 'Pendiente',
-        fechaCobranza: '2024-09-01'
-      },      {
-        nroSolicitud: 3,
-        nombre: 'Carlos',
-        apellido: 'García',
-        docIdentidad: '11223344C',
-        status: 'Pendiente',
-        fechaCobranza: '2024-09-01'
-      },      {
-        nroSolicitud: 3,
-        nombre: 'Carlos',
-        apellido: 'García',
-        docIdentidad: '11223344C',
-        status: 'Pendiente',
-        fechaCobranza: '2024-09-01'
-      },      {
-        nroSolicitud: 3,
-        nombre: 'Carlos',
-        apellido: 'García',
-        docIdentidad: '11223344C',
-        status: 'Pendiente',
-        fechaCobranza: '2024-09-01'
-      },      {
-        nroSolicitud: 3,
-        nombre: 'Carlos',
-        apellido: 'García',
-        docIdentidad: '11223344C',
-        status: 'Pendiente',
-        fechaCobranza: '2024-09-01'
-      },      {
-        nroSolicitud: 3,
-        nombre: 'Carlos',
-        apellido: 'García',
-        docIdentidad: '11223344C',
-        status: 'Pendiente',
-        fechaCobranza: '2024-09-01'
-      },      {
-        nroSolicitud: 3,
-        nombre: 'Carlos',
-        apellido: 'García',
-        docIdentidad: '11223344C',
-        status: 'Pendiente',
-        fechaCobranza: '2024-09-01'
-      },
-    ];
+  displayedColumns: string[] = ['nroSolicitud', 'Nombre', 'Apellido', 'Doc. identidad', 'Status', 'Fecha de Cobranza', 'acciones'];
+  dataSource = ELEMENT_DATA;
 
-    this.totalPages = Math.ceil(this.deudores.length / this.pageSize);
-    this.paginatedDeudores = this.paginate(this.deudores, this.pageSize, this.currentPage);
-  }
-
-  paginate(array: any[], pageSize: number, pageNumber: number): any[] {
-    return array.slice(pageNumber * pageSize, (pageNumber + 1) * pageSize);
-  }
-
-  goToPage(pageNumber: number): void {
-    if (pageNumber >= 0 && pageNumber < this.totalPages) {
-      this.currentPage = pageNumber;
-      this.paginatedDeudores = this.paginate(this.deudores, this.pageSize, this.currentPage);
-    }
-  }
-
-  nextPage(): void {
-    this.goToPage(this.currentPage + 1);
-  }
-
-  previousPage(): void {
-    this.goToPage(this.currentPage - 1);
-  }
 }
